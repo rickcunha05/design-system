02bb9f3b286a584c5273a8ddd67dda173132c460
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2024-05-01 22:21:57 -0300

    Documentação dos tokens faltantes 🚀

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import type {StoryObj, Meta} from '@storybook/react'
 import {Button, ButtonProps} from '@lhc-ui/react'
 
 export default {
-  title:'Button',
+  title:'Form/Button',
   component: Button,
 } as Meta<ButtonProps>
 
