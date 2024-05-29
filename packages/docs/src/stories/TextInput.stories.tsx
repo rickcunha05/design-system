c0376ba69989c6d028c00fad4c4f7803cd7440e5
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2024-05-28 21:54:33 -0300

    component input

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,37 @@
+import { type StoryObj, type Meta } from "@storybook/react";
+import { Box, Text, TextInput, TextInputProps, TextProps } from "@lhc-ui/react";
+
+export default {
+  title: "Form/Text Input",
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: "flex", flexDirection: "column", gap: "$2" }}
+        >
+          <Text size="sm">Email address</Text>
+          {Story()}
+        </Box>
+      );
+    },
+  ],
+} as Meta<TextInputProps>;
+
+export const Primary: StoryObj<TextProps> = {
+  args: {
+    placeholder: "Type your name",
+  },
+};
+export const WithPrefix: StoryObj<TextProps> = {
+  args: {
+    prefix: "cal.com/",
+  },
+};
+export const Disabled: StoryObj<TextProps> = {
+  args: {
+    disabled: true,
+  },
+};
